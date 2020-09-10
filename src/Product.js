55ebe47ddc79321cc0470092d0e58c86a199908c
Randy Ramsaywack <ramsaywackr@gmail.com>
2020-09-09 23:28:59 -0400

    Added React Context API

--- src/Product.js
+++ src/Product.js
@@ -1,13 +1,29 @@
 import React, { useState } from "react";
 import "./Product.css";
 import StarRatings from "react-star-ratings";
+import { useStateValue } from "./StateProvider";
 
 function Product({ id, title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
   const [userRating, setuserRating] = useState(rating);
 
   const changeRating = (newRating) => {
     setuserRating(newRating);
   };
+
+  const addToBasket = () => {
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+
   return (
     <div className="product">
       <div className="product__info">
@@ -28,7 +44,7 @@ function Product({ id, title, image, price, rating }) {
         </div>
         <img src={image} alt="No Image" />
       </div>
-      <button>Add to Basket</button>
+      <button onClick={addToBasket}>Add to Basket</button>
     </div>
   );
 }
--- src/StateProvider.js
+++ src/StateProvider.js
@@ -2,9 +2,9 @@ import React, { createContext, useContext, useReducer } from "react";
 
 export const StateContext = createContext();
 
-export const StateProvider = ({ reducer, initialState, childen }) => (
+export const StateProvider = ({ reducer, initialState, children }) => (
   <StateContext.Provider value={useReducer(reducer, initialState)}>
-    {childen}
+    {children}
   </StateContext.Provider>
 );
 
